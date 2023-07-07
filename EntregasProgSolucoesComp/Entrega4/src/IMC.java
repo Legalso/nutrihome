f118ce3902382d0e191c4d91112bc4619d072fc1
CaioP <caiopatriotabarros@gmail.com>
2023-07-07 12:31:56 -0300

    organização das pastas
    Excluí arquivos desnecessários e organizei outros em pastas. Agora começa a fase de reestruturação do site.

--- EntregasProgSolucoesComp/Entrega4/src/IMC.java
+++
@@ -1,6 +0,0 @@
-public class IMC {
-    public static double calcular(Pessoa pessoa){
-        double imc = pessoa.getPeso() / (pessoa.getAltura()* pessoa.getAltura());
-        return imc;
-    }
-}
