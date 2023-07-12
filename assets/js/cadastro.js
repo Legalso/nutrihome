cccc3667ff0137e934b7138d1171000aa54b5f0d
Mech24689 <gabrieltrem102030@gmail.com>
2023-07-11 23:09:35 -0300

    Alterações na tela criar conta e configurações.

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -3,28 +3,36 @@ let selectedRole = null;
 function selectRole(role) {
   const pacienteContainer = document.getElementById('paciente-container');
   const nutriContainer = document.getElementById('nutri-container');
+  const textNutri = document.getElementById('textNutri')
+  const textPaciente = document.getElementById('textPaciente')
 
   if (role === 'paciente') {
     if (selectedRole === 'paciente') {
       // Deselecionar paciente
       selectedRole = null;
       pacienteContainer.classList.remove('selected');
+      textPaciente.classList.remove('underline')
     } else {
       // Selecionar paciente
       selectedRole = 'paciente';
       pacienteContainer.classList.add('selected');
+      textPaciente.classList.add('underline')
       nutriContainer.classList.remove('selected');
+      textNutri.classList.remove('underline')
     }
   } else if (role === 'nutri') {
     if (selectedRole === 'nutri') {
       // Deselecionar nutri
       selectedRole = null;
       nutriContainer.classList.remove('selected');
+      textNutri.classList.remove('underline')
     } else {
       // Selecionar nutri
       selectedRole = 'nutri';
       nutriContainer.classList.add('selected');
+      textNutri.classList.add('underline')
       pacienteContainer.classList.remove('selected');
+      textPaciente.classList.remove('underline')
     }
   }
 }
