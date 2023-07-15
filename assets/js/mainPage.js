72439d922fbe05ee3c90de7e9cce76bd8a51550d
Mech24689 <gabrieltrem102030@gmail.com>
2023-07-15 12:40:00 -0300

    Ajuste do bug de resultado de imc e hidratação na tela mainPage

--- assets/js/mainPage.js
+++ assets/js/mainPage.js
@@ -4,5 +4,10 @@ let metaAgua = document.querySelector("#meta-agua")
 let peso = localStorage.getItem('peso')
 let imc = localStorage.getItem('imc')
 
-metaAgua.innerHTML = 35 * parseInt(peso) + "ml"
-textIMC.innerHTML = imc
+if(imc == null){
+    metaAgua.innerHTML = "0 ml"
+    textIMC.innerHTML = "0"
+}else{
+    metaAgua.innerHTML = 35 * parseInt(peso) + "ml"
+    textIMC.innerHTML = imc
+}
